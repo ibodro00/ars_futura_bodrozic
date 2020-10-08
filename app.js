e8c4f002674eadd25323e0e434a141514f188b41
ivan <ibodro00@fesb.hr>
2020-10-08 19:15:41 +0200

    prettier

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const listRoute = require('./routes/listRoute');
 const newlistRoute= require('./routes/newlistRoute');
 const cookieParser = require('cookie-parser');
 
+
 mongoose.connect('mongodb://IvanB:arsfuturatest@cluster0-shard-00-00.my9yh.mongodb.net:27017,cluster0-shard-00-01.my9yh.mongodb.net:27017,cluster0-shard-00-02.my9yh.mongodb.net:27017/Too-doo?ssl=true&replicaSet=atlas-mxmouq-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false})
     .then(result => {
         app.listen(process.env.PORT || 4000);
